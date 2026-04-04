c7f69f3642999edb856df65b5cec56c01a9b67ef
Suriya G <suriyanarayanang16@gmail.com>
2026-04-04 21:54:59 +0530

    14th Commit

--- js/squad.js
+++ js/squad.js
@@ -283,8 +283,8 @@ function renderSquad(){
     return '<div class="'+cardCls+'" style="--ipl-color:'+color+';animation-delay:'+(i*0.05)+'s"'+
       (p.availability_note?' title="'+UI.esc(p.availability_status+': '+p.availability_note)+'"':'')+'>' +
       '<div class="player-card-header">' +
-        roleHtml +
         osHtml +
+        roleHtml +
       '</div>' +
       '<div class="player-card-body">' +
         imgTag(displayImg, displayName, 'player-avatar') +
