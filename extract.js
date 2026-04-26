7633c8e049a3c2b301d11e1df2f54d03286b8e31
Suriya G <suriyanarayanang16@gmail.com>
2026-04-26 13:40:08 +0530

    39th Commit

--- extract.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-const html = fs.readFileSync('summary.html', 'utf8');
-// Find the last <script>...</script>
-const scripts = html.match(/<script>([\s\S]*?)<\/script>/g);
-if (!scripts) { console.log('No script'); process.exit(0); }
-const lastScript = scripts[scripts.length-1];
-const code = lastScript.replace(/^<script>/, '').replace(/<\/script>$/, '');
-// Write to temp
-fs.writeFileSync('_temp_check.js', code, 'utf8');
